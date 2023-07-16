ae8c97bce9f5d3cbac3640ee51eb8bf9dbd584fb
cathands <cathandsservice@gmail.com>
2023-07-16 20:48:09 +0900

    style changes

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,86 +1,92 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
 export const StopWatch = () => {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef<number | null>(null);
 
-  useEffect(() => {
-    let intervalId: string | number | NodeJS.Timer | undefined;
-
+  const startStopTimer = () => {
     if (isRunning) {
-      intervalId = setInterval(() => {
-        setTime(prevTime => prevTime + 1);
+      clearInterval(timerRef.current!);
+    } else {
+      const startTime = Date.now() - time * 1000;
+      timerRef.current = window.setInterval(() => {
+        const elapsed = (Date.now() - startTime) / 1000;
+        setTime(elapsed);
       }, 1000);
     }
-
-    return () => clearInterval(intervalId);
-  }, [isRunning]);
+    setIsRunning(!isRunning);
+  };
 
   const handleReset = () => {
+    clearInterval(timerRef.current!);
     setTime(0);
     setIsRunning(false);
   };
 
-  const formatTime = (timeInSeconds: number) => {
-    const hours = Math.floor(timeInSeconds / 1440)
-      .toString()
-      .padStart(2, '0');
-    const minutes = Math.floor(timeInSeconds / 60)
-      .toString()
-      .padStart(2, '0');
-    const seconds = (timeInSeconds % 60)
-      .toString()
-      .padStart(2, '0');
-
+  const formatTime = (time: number) => {
+    const hours = Math.floor(time / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+    const seconds = Math.floor(time % 60).toString().padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
   };
+
+  const Container = styled.div`
+    width: 600px;
+    margin: 80px auto 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
 
   const H1 = styled.h1`
     font-size: 70px;
     text-align: center;
   `;
 
-  const Container = styled.div`
-    width: 600px;
-    margin: 80px auto 0;
-  `;
+  const Desc = styled.p`
+  font-size: 24px;
+  text-align: center;
+`;
 
-  const Time = styled.div`
+  const Time = styled.button`
     width: 100%;
     line-height: 2;
     border: 2px double #000;
     border-radius: 10px;
+    color: #000;
     font-size: 72px;
     font-weight: bold;
     text-align: center;
-  `;
-
-  const ButtonWrapper = styled.div`
-    display: flex;
-    justify-content: center;
+    transition: ".3s";
   `;
 
   const Button = styled.button`
     width: 200px;
     height: 50px;
-    margin: 20px 10px 0;
+    margin-top: 20px;
     background-color: #fff;
     border-radius: 5px;
     font-size: 24px;
   `;
 
+  const active = {
+    backgroundColor: "#fff",
+    transition: ".3s",
+  };
+
+  const stop = {
+    backgroundColor: "#dbdbdbc7",
+    transition: ".3s",
+  };
+
   return (
     <Container>
-      <H1>Stopwatch App</H1>
-      <Time>{formatTime(time)}</Time>
-      <ButtonWrapper>
-        {!isRunning
-          ? <Button onClick={()=>{setIsRunning(true)}}>Start</Button>
-          : <Button onClick={()=>{setIsRunning(false)}}>Stop</Button>
-        }
-        <Button onClick={handleReset}>Reset</Button>
-      </ButtonWrapper>
+      <H1>StopWatch</H1>
+      <Desc>click to Start or Stop </Desc>
+      <Time style={isRunning ? active : stop} onClick={startStopTimer}>{formatTime(time)}</Time>
+      <Button onClick={handleReset}>Reset</Button>
     </Container>
   );
 }
