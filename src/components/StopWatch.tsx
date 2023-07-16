687b72bba6831d6664491c2c0b55c7e393a71d38
cathands <cathandsservice@gmail.com>
2023-07-16 22:11:08 +0900

    refactoring

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,96 +1,98 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
+import '../index'
 
 export const StopWatch = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
   const timerRef = useRef<number | null>(null);
 
-  const startStopTimer = () => {
-    if (isRunning) {
-      clearInterval(timerRef.current!);
-    } else {
-      const startTime = Date.now() - time * 1000;
-      timerRef.current = window.setInterval(() => {
-        const elapsed = (Date.now() - startTime) / 1000;
-        setTime(elapsed);
-      }, 1000);
-    }
-    setIsRunning(!isRunning);
-  };
+  // const Timer = () => {
+  //   if (isRunning) {
+  //     clearInterval(timerRef.current!);
+  //     timerRef.current = null;
+  //   } else {
+  //     const startTime = Date.now() - time * 1000;
+  //     timerRef.current = window.setInterval(() => {
+  //       const elapsed = (Date.now() - startTime) / 1000;
+  //       setTime(elapsed);
+  //     }, 100);
+  //   }
+  //   setIsRunning(!isRunning);
+  // };
 
-  const handleReset = () => {
-    clearInterval(timerRef.current!);
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime + 0.01);
+      }, 10);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const handleStart = useCallback(() => {
+    setIsRunning(true);
+  }, []);
+
+  const handleStop = useCallback(() => {
+    setIsRunning(false);
+  }, []);
+
+  const handleReset = useCallback(() => {
     setTime(0);
     setIsRunning(false);
-  };
+  }, []);
 
   const padTime = (targetTime: number, digits: number) => {
     return targetTime.toString().padStart(digits, '0');
   };
 
-  const formatTime = (time: number) => {
+  const formatTime = (): string => {
     const hours = padTime(Math.floor(time / 3600), 2);
     const minutes = padTime(Math.floor((time % 3600) / 60), 2);
     const seconds = padTime(Math.floor(time % 60), 2);
-    return `${hours}:${minutes}:${seconds}`;
+    const milliseconds = padTime(Math.floor((time % 1) * 100), 2);
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
   };
 
-  const Container = styled.div`
-    width: 600px;
-    margin: 80px auto 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `;
-
-  const H1 = styled.h1`
-    font-size: 70px;
-    text-align: center;
-  `;
-
-  const Desc = styled.p`
-  font-size: 24px;
-  text-align: center;
-`;
-
-  const Time = styled.button`
-    width: 100%;
-    line-height: 2;
-    border: 2px double #000;
-    border-radius: 10px;
-    color: #000;
-    font-size: 72px;
-    font-weight: bold;
-    text-align: center;
-    transition: ".3s";
-  `;
-
-  const Button = styled.button`
-    width: 200px;
-    height: 50px;
-    margin-top: 20px;
-    background-color: #fff;
-    border-radius: 5px;
-    font-size: 24px;
-  `;
-
   const active = {
+    width: "100%",
+    lineHeight: 2,
+    border: "2px double #000",
+    borderRadius: "10px",
     backgroundColor: "#fff",
-    transition: ".3s",
   };
 
   const stop = {
+    width: "100%",
+    lineHeight: 2,
+    border: "2px double #000",
+    borderRadius: "10px",
     backgroundColor: "#dbdbdbc7",
-    transition: ".3s",
   };
 
   return (
-    <Container>
-      <H1>StopWatch</H1>
-      <Desc>click to Start or Stop </Desc>
-      <Time style={isRunning ? active : stop} onClick={startStopTimer}>{formatTime(time)}</Time>
-      <Button onClick={handleReset}>Reset</Button>
-    </Container>
+    <div className='container'>
+      <h1 className='h1'>StopWatch</h1>
+      <p className='desc'>click to Start or Stop </p>
+      <button
+        className='timer'
+        style={isRunning ? active : stop}
+        onClick={isRunning? handleStop: handleStart}
+      >
+        {formatTime()}
+      </button>
+      <button
+        className='reset'
+        onClick={handleReset}
+      >
+        Reset
+      </button>
+    </div>
   );
 }
